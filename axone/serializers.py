b97896da3e9fcaf454148799f92ef80d0820e9d8
Pierre-Yves BRULIN <40915900+PYBrulin@users.noreply.github.com>
2023-11-26 10:54:35 +0100

    initial commit

---
+++ axone/serializers.py
@@ -0,0 +1,41 @@
+import json
+from typing import Final, Protocol
+
+NULL_BYTE: Final = b"\x00"
+
+
+class SerializationError(ValueError):
+    def __init__(self, data: dict) -> None:
+        super().__init__(f"Failed to serialize data: {data!r}")
+
+
+class DeserializationError(ValueError):
+    def __init__(self, data: bytes) -> None:
+        super().__init__(f"Failed to deserialize data: {data!r}")
+
+
+class SharedMemoryDictSerializer(Protocol):
+    def dumps(self, obj: dict) -> bytes:
+        ...
+
+    def loads(self, data: bytes) -> dict:
+        ...
+
+
+class JSONSerializer:
+    __slots__ = ()
+    encoder = json.JSONEncoder
+    decoder = json.JSONDecoder
+
+    def dumps(self, obj: dict) -> bytes:
+        try:
+            return json.dumps(obj, cls=self.encoder).encode() + NULL_BYTE
+        except (ValueError, TypeError):
+            raise SerializationError(obj)
+
+    def loads(self, data: bytes) -> dict:
+        data = data.split(NULL_BYTE, 1)[0]
+        try:
+            return json.loads(data, cls=self.decoder)
+        except json.JSONDecodeError:
+            raise DeserializationError(data)
