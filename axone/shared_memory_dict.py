a483e851137aaff731acae09690887dddd3acae9
Pierre-Yves BRULIN <pierre-yves.brulin@estaca.fr>
2023-11-28 12:56:38 +0100

    Remove debug logs and deprecation warning

--- axone/shared_memory_dict.py
+++ axone/shared_memory_dict.py
@@ -77,14 +77,7 @@ class SharedMemoryDict:
     def clear(self) -> None:
         self._save_memory({})
 
-    def popitem(self, last: Optional[bool] = None) -> Any:
-        if last is not None:
-            warnings.warn(
-                "The 'last' parameter will be removed in future versions. "
-                "The 'popitem' function now always returns last inserted.",
-                DeprecationWarning,
-                stacklevel=2,
-            )
+    def popitem(self) -> Any:
         with self._modify_db() as db:
             return db.popitem()
 
@@ -96,58 +89,46 @@ class SharedMemoryDict:
         self._save_memory(db)
 
     def __getitem__(self, key: str) -> Any:
-        logging.debug(f"__getitem__ {key}")
         return self._read_memory()[key]
 
     def __setitem__(self, key: str, value: Any) -> None:
-        logging.debug(f"__setitem__ {key} {value}")
         with self._modify_db() as db:
             db[key] = value
 
     def __len__(self) -> int:
-        logging.debug(f"__len__")
         return len(self._read_memory())
 
     def __delitem__(self, key: str) -> None:
-        logging.debug(f"__delitem__ {key}")
         with self._modify_db() as db:
             del db[key]
 
     def __iter__(self) -> Iterator:
-        logging.debug(f"__iter__")
         return iter(self._read_memory())
 
-    def __reversed__(self):
-        logging.debug(f"__reversed__")
+    def __reversed__(self) -> reversed[str]:
         return reversed(self._read_memory())
 
     def __del__(self) -> None:
         self.cleanup()
 
     def __contains__(self, key: str) -> bool:
-        logging.debug(f"__contains__ {key}")
         return key in self._read_memory()
 
     def __eq__(self, other: Any) -> bool:
-        logging.debug(f"__eq__ {other}")
         return self._read_memory() == other
 
     def __ne__(self, other: Any) -> bool:
-        logging.debug(f"__ne__ {other}")
         return self._read_memory() != other
 
     if sys.version_info > (3, 8):
 
-        def __or__(self, other: Any):
-            logging.debug(f"__or__ {other}")
+        def __or__(self, other: Any) -> Any:
             return self._read_memory() | other
 
-        def __ror__(self, other: Any):
-            logging.debug(f"__ror__ {other}")
+        def __ror__(self, other: Any) -> Any:
             return other | self._read_memory()
 
-        def __ior__(self, other: Any):
-            logging.debug(f"__ior__ {other}")
+        def __ior__(self, other: Any) -> Any:
             with self._modify_db() as db:
                 db |= other
                 return db
@@ -159,35 +140,28 @@ class SharedMemoryDict:
         return repr(self._read_memory())
 
     def get(self, key: str, default: Optional[Any] = None) -> Any:
-        logging.debug(f"get {key} {default}")
         return self._read_memory().get(key, default)
 
     def keys(self) -> KeysView[Any]:
-        logging.debug(f"keys")
         return self._read_memory().keys()
 
     def values(self) -> ValuesView[Any]:
-        logging.debug(f"values")
         return self._read_memory().values()
 
     def items(self) -> ItemsView:
-        logging.debug(f"items")
         return self._read_memory().items()
 
     def pop(self, key: str, default: Optional[Any] = NOT_GIVEN):
-        logging.debug(f"pop {key} {default}")
         with self._modify_db() as db:
             if default is NOT_GIVEN:
                 return db.pop(key)
             return db.pop(key, default)
 
     def update(self, other=(), /, **kwds):
-        logging.debug(f"update {other} {kwds}")
         with self._modify_db() as db:
             db.update(other, **kwds)
 
     def setdefault(self, key: str, default: Optional[Any] = None):
-        logging.debug(f"setdefault {key} {default}")
         with self._modify_db() as db:
             return db.setdefault(key, default)
 
