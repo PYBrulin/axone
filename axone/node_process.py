68985d8d5aede7651326eb74c297ff6eb98fcea9
Pierre-Yves BRULIN <pierre-yves.brulin@estaca.fr>
2023-12-17 13:44:30 +0100

    Add initial NodeProcess

--- axone/node_process.py
+++ axone/node_process.py
@@ -1,23 +1,25 @@
-from node import Node
+from multiprocessing import Process
+
+from .node import Node
 
 
 class NodeProcess(Node):
     """NodeProcess is a node that executes inside a process."""
 
-    def __init__(self, name, process):
+    def __init__(self, name: str, **kwargs):
         """Initialize the node."""
-        super().__init__(name)
-        self.process = process
+        super().__init__(name, **kwargs)
 
     def start(self):
         """Start the node."""
-        self.process.start()
+        self._executor = Process(target=self._federated_server)
+        self._executor.start()
 
     def stop(self):
         """Stop the node."""
-        self.process.terminate()
-        self.process.join()
+        self._executor.terminate()
+        self._executor.join()
 
     def join(self):
         """Join the node."""
-        self.process.join()
+        self._executor.join()
