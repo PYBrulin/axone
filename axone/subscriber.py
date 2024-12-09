0af0280db3d725140e49f2d9fc872aec607c6ff9
Pierre-Yves BRULIN <40915900+PYBrulin@users.noreply.github.com>
2024-12-09 16:33:10 +0100

    Allow reconnection

--- axone/subscriber.py
+++ axone/subscriber.py
@@ -20,6 +20,7 @@ class Subscription:
         retry_interval: float = 1.0,  # Retry interval in seconds
         max_retries: int = 10,  # Maximum number of retries
         multicast_group: str = "224.1.1.1",  # Multicast group address
+        timeout: float = 1.0,  # Timeout for socket operations
     ) -> None:
         self._name: str = topic_name
         self._request_rate: float = rate
@@ -39,6 +40,7 @@ class Subscription:
         self._multicast_group = multicast_group
         self._multicast_port = 0
         self._joined_multicast = False  # Flag to indicate if multicast group is joined
+        self._timeout = timeout
 
         self._zeroconf = Zeroconf()
         self._service_browser = ServiceBrowser(self._zeroconf, "_axone._udp.local.", handlers=[self._on_service_state_change])
@@ -53,8 +55,8 @@ class Subscription:
             except FileNotFoundError:
                 logging.error(f"Shared memory {self._uuid} not found")
                 self._memory = None
-        # elif self._method == "socket":
-        #     self._create_socket()
+        elif self._method == "socket":
+            self._create_socket_with_retries()
 
         # TODO : Implement unlink, close, etc for the SHM
 
@@ -62,29 +64,40 @@ class Subscription:
         if state_change == ServiceStateChange.Added:
             info = zeroconf.get_service_info(service_type, name)
             if info and info.properties.get(b'name').decode('utf-8') == self._name:
-                self._multicast_group = socket.inet_ntoa(info.addresses[0])
-                self._multicast_port = info.port
-                logging.info(f"Discovered service {name} at {self._multicast_group}:{self._multicast_port}")
-                if self._method == "socket" and not self._joined_multicast:
-                    self._create_socket()
+                new_multicast_group = socket.inet_ntoa(info.addresses[0])
+                new_multicast_port = info.port
+                logging.info(f"Discovered service {name} at {new_multicast_group}:{new_multicast_port}")
+                if self._method == "socket":
+                    if new_multicast_group != self._multicast_group or new_multicast_port != self._multicast_port:
+                        self._multicast_group = new_multicast_group
+                        self._multicast_port = new_multicast_port
+                        self._create_socket_with_retries()
+        elif state_change == ServiceStateChange.Removed:
+            info = zeroconf.get_service_info(service_type, name)
+            if info and info.properties.get(b'name').decode('utf-8') == self._name:
+                logging.info(f"Service {name} removed")
+                self._joined_multicast = False
 
-    # def _create_socket_with_retries(self) -> None:
-    #     retries = 0
-    #     while retries < self._max_retries:
-    #         try:
-    #             self._create_socket()
-    #             return
-    #         except OSError as e:
-    #             logging.warning(f"Failed to bind socket on attempt {retries + 1}/{self._max_retries}: {e}")
-    #             retries += 1
-    #             time.sleep(self._retry_interval)
-    #     raise RuntimeError(f"Failed to bind socket after {self._max_retries} attempts")
+    def _create_socket_with_retries(self) -> None:
+        retries = 0
+        while retries < self._max_retries:
+            try:
+                self._create_socket()
+                return
+            except OSError as e:
+                logging.warning(f"Failed to bind socket on attempt {retries + 1}/{self._max_retries}: {e}")
+                retries += 1
+                time.sleep(self._retry_interval)
+        raise RuntimeError(f"Failed to bind socket after {self._max_retries} attempts")
 
     def _create_socket(self) -> None:
         if self._multicast_port == 0:
             return
+        if self._socket:
+            self._socket.close()
         self._socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
         self._socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self._socket.settimeout(self._timeout)
         self._socket.bind(('', self._multicast_port))
 
         # Tell the kernel that we want to join the multicast group
@@ -153,7 +166,7 @@ class Subscription:
             # Get the message from the shared memory
             encoded = bytes(self._memory.buf[:])
         elif self._method == "socket":
-            encoded = self._subscribe_socket()
+            encoded = self._subscribe_socket_with_retries()
 
         if encoded:
             self._topic.decode(encoded)
@@ -166,20 +179,20 @@ class Subscription:
 
             # Note: Calling the callbacks is done in the main loop not here
 
-    # def _subscribe_socket_with_retries(self) -> Optional[bytes]:
-    #     if self._socket is None:
-    #         return None
-    #     retries = 0
-    #     while retries < self._max_retries:
-    #         try:
-    #             return self._subscribe_socket()
-    #         except OSError as e:
-    #             logging.warning(f"Socket error on attempt {retries + 1}/{self._max_retries}: {e}")
-    #             retries += 1
-    #             time.sleep(self._retry_interval)
-    #             self._create_socket()
-    #     logging.error(f"Failed to receive data after {self._max_retries} attempts")
-    #     return None
+    def _subscribe_socket_with_retries(self) -> Optional[bytes]:
+        if self._socket is None:
+            return None
+        retries = 0
+        while retries < self._max_retries:
+            try:
+                return self._subscribe_socket()
+            except (OSError, socket.timeout) as e:
+                logging.warning(f"Socket error on attempt {retries + 1}/{self._max_retries}: {e}")
+                retries += 1
+                time.sleep(self._retry_interval)
+                self._create_socket_with_retries()
+        logging.error(f"Failed to receive data after {self._max_retries} attempts")
+        return None
 
     def _subscribe_socket(self) -> Optional[bytes]:
         if self._socket is None:
@@ -187,7 +200,7 @@ class Subscription:
         try:
             data, _ = self._socket.recvfrom(2048)  # Adjust buffer size as needed
             return data
-        except OSError as e:
+        except (OSError, socket.timeout) as e:
             logging.error(f"Socket error: {e}")
             raise
 
