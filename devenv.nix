862c7526c4196c1208ad01313e2e22e711e72ce9
Pierre-Yves BRULIN <40915900+PYBrulin@users.noreply.github.com>
2025-02-14 14:24:36 +0100

    Add Devenv configuration and related files for Python environment setup

---
+++ devenv.nix
@@ -0,0 +1,12 @@
+{ pkgs, ... }:
+
+{
+  languages.python = {
+    enable = true;
+    version = "3.10.10";
+    venv = {
+      enable = true;
+      # TODO: It is not possible to use a pyproject.toml file to specify dependencies. I am putting this on hold for now. Install dependencies manually.
+    };
+  };
+}
