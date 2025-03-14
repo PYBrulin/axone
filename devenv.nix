730c6f38f81f9725010fadd65a66f960f55ec0ec
Pierre-Yves BRULIN <40915900+PYBrulin@users.noreply.github.com>
2025-03-14 11:51:11 +0100

    Update devenv

--- devenv.nix
+++ devenv.nix
@@ -1,12 +1,18 @@
-{ pkgs, ... }:
-
-{
+{pkgs, ...}: {
   languages.python = {
     enable = true;
     version = "3.10.10";
     venv = {
       enable = true;
       # TODO: It is not possible to use a pyproject.toml file to specify dependencies. I am putting this on hold for now. Install dependencies manually.
+      requirements = ''
+        crc
+        filelock
+        netifaces
+        portalocker ; platform_system == "Windows"
+        zeroconf
+        . # This package
+      '';
     };
   };
 }
